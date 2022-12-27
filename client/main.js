44b6c4bf83d8dbe43d31fc4983e7e93fe2489d00
third-park <qkrtpaud01@gmail.com>
2022-12-27 13:52:21 +0900

    [생성] 초기설정

---
+++ client/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+console.log(window);
+
+
+
+
+
+
+
+
+
+
---
+++ server/index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+const liveServer = require('live-server');
+
+
+const params = {
+  host: 'localhost',
+  port: 3000,
+  open: false,
+  root: './client'
+};
+
+liveServer.start(params);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
