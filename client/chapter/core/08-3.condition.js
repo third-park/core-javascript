651eb7025aa16174dd7b80d3bba7f6bf905da905
third-park <qkrtpaud01@gmail.com>
2022-12-30 00:25:03 +0900

    toLowerCase

--- client/chapter/core/08-3.condition.js
+++ client/chapter/core/08-3.condition.js
@@ -12,9 +12,9 @@ const MORNING    = '아침',
       LATE_NIGHT = '심야',
       DAWN       = '새벽';
 
-// let thisTime = prompt('하루루틴을 알려드릴게요. 지금 시간대는 언제인가요?', '점심');
+/* let thisTime = prompt('하루루틴을 알려드릴게요. 지금 시간대는 언제인가요?', '점심');
 
-/* switch (thisTime) {
+switch (thisTime) {
   case '아침': alert('이듬 강의를 듣는다.'); break;
   case '점심': alert('밥먹고 파워낮잠 때린다.'); break;
   case '저녁': alert('복습하고 스터디를 진행한다.'); break;
@@ -54,7 +54,7 @@ const MORNING    = '아침',
 // 6 = '토'
 
 
-function getRandom(n) {
+/* function getRandom(n) {
   return Math.round(Math.random() * n);
 }
 
@@ -74,4 +74,24 @@ function getDay(dayValue) {
 let result = (getDay(getRandom(6)));
 
 console.log(result);
-//이렇게 적을떄 왜 break문이 필요없는가? => 함수는 return을 만나게 되면 결과 내뱉고 실행을 종료하기 때문에 break문이 필요가 없는 것
+ *///이렇게 적을떄 왜 break문이 필요없는가? => 함수는 return을 만나게 되면 결과 내뱉고 실행을 종료하기 때문에 break문이 필요가 없는 것
+
+let login = prompt('아이디 주세요', '');
+let loginLowerCase = login.toLowerCase();
+
+if (login === ' ' || login === null) {
+  console.log('취소되었습니다.');
+} else if (loginLowerCase === 'sem') {
+  let pw = prompt('비밀번호를 치세요');
+  if (pw === ' ' || pw === null) {
+    console.log('잘못된 양식입니다.');
+  } else if (pw === '1234') {
+    console.log('로그인 완료');
+  } else {
+    console.log('없는 비밀번호 입니다.');
+  }
+} else {
+  console.log('없는 아이디 입니다.');
+}
+
+
